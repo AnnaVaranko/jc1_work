a6fe4659e4dd7a631b98bb5658214d5740511bf9
AnnaVaranko <avoronk@yandex.ru>
2022-03-01 21:17:15 +0300

    My second calculator

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String command = "C";
+        while (command.equals("C")) {
+            Scanner scanner = new Scanner(System.in);
+
+            Arguments arguments = readArguments();
+            doCanculation(arguments);
+            command = scanner.next();
+        }
+    }
+
+    private static Arguments readArguments(){
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Pleas enter first number: ");
@@ -16,10 +27,14 @@ public class Main {
         System.out.print("Pleas enter first number: ");
         int secondNumber = scanner.nextInt();
 
-        Calculator calc = new Calculator();
+        return new Arguments(firstNumber, operation, secondNumber);
+    }
+
+    private static void doCanculation(Arguments arguments){
         final double result =
-                calc.calculate(firstNumber,secondNumber, operation.charAt(0));
-        System.out.println("Result of " + firstNumber + " " + operation+ " " + secondNumber
+                calc.calculate(firstNumber, secondNumber, operation.charAt(0));
+        System.out.println("Result of " + firstNumber + " " + operation.charAt(0) + " " + secondNumber
                 + " is " + result);
+        System.out.print("Pleas enter [C] to continue or [E] to exit ");
     }
 }
